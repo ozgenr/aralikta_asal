9d9b8972bf4a13d4f50478590a4ae768e18ca7eb
ozgenr <155058207+ozgenr@users.noreply.github.com>
2024-05-19 11:54:04 +0300

    aralikta-asal2

--- aralikta_asal.c
+++ aralikta_asal.c
@@ -2,19 +2,22 @@
 #include <stdlib.h>
 
 int main(int argc, char *argv[]) {
-	int baslangic,bitis,i,j,bayrak=1;
+	int baslangic,bitis,i,j,bayrak=1,gecici;
 	printf("baslangic ve bitis degerini sirasiyla giriniz:");
 	scanf("%d%d",&baslangic,&bitis);
 	for(i=baslangic+1;i<bitis;i++)
-	{  for(j=2;j<i&&bayrak==1;j++)
+	{  for(j=2;j<(gecici=i)&&bayrak==1;j++)
 	   {  if((i%j)==0)
 	      bayrak=0;
+	      else
+	      gecici=gecici/j;
+	      
 	   }
 	   if(bayrak==1&&i!=1)
 	   printf("%d ",i);
 	   bayrak=1;
-	
+
 	}
-	
+
 	return 0;
 }
